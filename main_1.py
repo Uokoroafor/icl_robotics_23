f00e67472c4436164f737401ca9cdf4f23e2f135
jd22 <joshan.dooki22@imperial.ac.uk>
2023-01-26 14:27:42 +0000

    first push

---
+++ main_1.py
@@ -0,0 +1,37 @@
+#import modules
+import brickpi 
+import time
+BP = brickpi3.BrickPi3()
+
+
+interface=brickpi.Interface()
+interface.initialize()
+
+# Motor control code #
+# Start
+
+
+
+# End
+
+
+
+# Distance and Rotation Calibration for Accurate Driving 
+#Start
+
+def forward_40():
+    pass
+
+def backward_40():
+    pass
+
+
+def clockwise_90():
+    pass
+
+def anti_clockwise_90():
+    pass
+
+
+
+#End
