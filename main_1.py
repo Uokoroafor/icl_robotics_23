1a6d4065162d86fbdbfbf7611c6a359c90fb1bb3
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-01-26 14:59:28 +0000

    updates to main.py

--- main_1.py
+++ main_1.py
@@ -1,25 +1,25 @@
-#import modules
-import brickpi 
+# import modules
+import BrickPi3
 import time
-BP = brickpi3.BrickPi3()
 
+BP = BrickPi3()
 
-interface=brickpi.Interface()
+interface = BP.Interface()
 interface.initialize()
+
 
 # Motor control code #
 # Start
 
 
-
 # End
 
 
-
-# Distance and Rotation Calibration for Accurate Driving 
-#Start
+# Distance and Rotation Calibration for Accurate Driving
+# Start
 
 def forward_40():
+    pass
 
 
 def backward_40():
@@ -29,9 +29,8 @@ def backward_40():
 def clockwise_90():
     pass
 
+
 def anti_clockwise_90():
     pass
 
-
-
-#End
+# End
